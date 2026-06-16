d9fb677f5ea9ec2c4b1c77fd0e3522372814a3fc
mahabc <revenck@gmail.com>
2026-06-16 07:59:14 +0200

    footer added

---
+++ footer.js
@@ -0,0 +1,9 @@
+fetch("../footer.html")
+  .then(r => {
+    if (!r.ok) throw new Error("footer.html not found");
+    return r.text();
+  })
+  .then(html => {
+    document.getElementById("footer-placeholder").innerHTML = html;
+  })
+  .catch(err => console.error(err));
